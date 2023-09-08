2ec6118f2bc440b432bc6ddf9ee11b12b7ce7ecf
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-09-08 12:09:42 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,138 +1,138 @@
+import os, cv2, torch, numpy as np
+from glob import glob
+from tqdm import tqdm
+from torch.autograd import Variable
+from model import U2NET, normPRED
 
-def get_video_frames(video_path, save_path):
+class VideoMaker():
     
-    import os, cv2
-    if os.path.isdir(save_path): print("Video frames are already obtained! Skipping this step..."); pass
-    else: 
-        # Create directory to save images
-        os.makedirs(save_path, exist_ok = True)
-        # Video capture
-        vidcap = cv2.VideoCapture(f"{video_path}")
-        # Read the capture
-        suc, im = vidcap.read()
+    def __init__(self, video_path, frame_save_path, sketch_save_path, device, checkpoint, video_save_path, save_name):
+        
+        self.video_path, self.frame_save_path, self.sketch_save_path= video_path, frame_save_path, sketch_save_path
+        self.device, self.checkpoint, self.video_save_path, self.save_name = device, checkpoint, video_save_path, save_name
+        
+    def get_video_frames(self):
 
-        count = 0
-        print("Obtaining frames from the video...")
-        while suc:
-            # Save the image
-            cv2.imwrite(f"{save_path}/frame{count}.jpg", im) 
-            # Read the next capture
+        if os.path.isdir(self.frame_save_path): print("Video frames are already obtained! Skipping this step..."); pass
+        else: 
+            # Create directory to save images
+            os.makedirs(self.frame_save_path, exist_ok = True)
+            # Video capture
+            vidcap = cv2.VideoCapture(f"{self.video_path}")
+            # Read the capture
             suc, im = vidcap.read()
-            count += 1
 
-        print(f"Total number of frames -> {count}")
+            count = 0
+            print("Obtaining frames from the video...")
+            while suc:
+                # Save the image
+                cv2.imwrite(f"{self.frame_save_path}/frame{count}.jpg", im) 
+                # Read the next capture
+                suc, im = vidcap.read()
+                count += 1
+
+            print(f"Total number of frames -> {count}")
 
 
-def get_sketches(path, save_path, device, checkpoint):
-    
-    import cv2, os, torch, numpy as np
-    from glob import glob
-    from tqdm import tqdm
-    from torch.autograd import Variable
-    from model import U2NET, normPRED
-    
-    if os.path.isdir(save_path): print("Sketch images are already obtained! Skipping this step..."); pass
-    else:
-        # Create directory to save images
-        os.makedirs(save_path, exist_ok = True)
+    def get_sketches(self):
 
-        net = U2NET(3,1)
-        state_dict = {}
-        torch_dict_sk = torch.load(checkpoint, map_location='cpu')
-        for key in torch_dict_sk:
-            if key.startswith('module'):
-                state_dict[key[7:]] = torch_dict_sk[key]
-            else:
-                state_dict[key] = torch_dict_sk[key]
+        if os.path.isdir(self.sketch_save_path): print("Sketch images are already obtained! Skipping this step..."); pass
+        else:
+            # Create directory to save images
+            os.makedirs(self.sketch_save_path, exist_ok = True)
 
-        net.load_state_dict(state_dict)
-        net.eval().to(device)
+            net = U2NET(3,1)
+            state_dict = {}
+            torch_dict_sk = torch.load(self.checkpoint, map_location='cpu')
+            for key in torch_dict_sk:
+                if key.startswith('module'):
+                    state_dict[key[7:]] = torch_dict_sk[key]
+                else:
+                    state_dict[key] = torch_dict_sk[key]
 
-        # Get image files
-        im_files = glob(f"{path}/*.jpg")
-        im_files.sort(key = os.path.getctime)
+            net.load_state_dict(state_dict)
+            net.eval().to(self.device)
 
-        print("Converting images to sketches...")
-        for idx, im_file in tqdm(enumerate(im_files)):
-            # input = cv2.cvtColor(cv2.imread(im_file), cv2.COLOR_BGR2GRAY)
-            input = cv2.imread(im_file)
+            # Get image files
+            im_files = glob(f"{self.frame_save_path}/*.jpg")
+            im_files.sort(key = os.path.getctime)
 
-            tmpImg = np.zeros((input.shape[0],input.shape[1],3))
+            print("Converting images to sketches...")
+            for idx, im_file in tqdm(enumerate(im_files)):
+                # input = cv2.cvtColor(cv2.imread(im_file), cv2.COLOR_BGR2GRAY)
+                input = cv2.imread(im_file)
 
-            input = input/np.max(input)
+                tmpImg = np.zeros((input.shape[0],input.shape[1],3))
 
-            tmpImg[:,:,0] = (input[:,:,2]-0.406)/0.225
-            tmpImg[:,:,1] = (input[:,:,1]-0.456)/0.224
-            tmpImg[:,:,2] = (input[:,:,0]-0.485)/0.229
+                input = input/np.max(input)
 
-            # convert BGR to RGB
-            tmpImg = tmpImg.transpose((2, 0, 1))
-            tmpImg = tmpImg[np.newaxis,:,:,:]
-            tmpImg = torch.from_numpy(tmpImg)
+                tmpImg[:,:,0] = (input[:,:,2]-0.406)/0.225
+                tmpImg[:,:,1] = (input[:,:,1]-0.456)/0.224
+                tmpImg[:,:,2] = (input[:,:,0]-0.485)/0.229
 
-            # convert numpy array to torch tensor
-            tmpImg = tmpImg.type(torch.FloatTensor)
-            tmpImg = tmpImg.to(device)
-        #     if torch.cuda.is_available():
-        #         tmpImg = Variable(tmpImg.cuda())
-        #     else:
-            tmpImg = Variable(tmpImg)
+                # convert BGR to RGB
+                tmpImg = tmpImg.transpose((2, 0, 1))
+                tmpImg = tmpImg[np.newaxis,:,:,:]
+                tmpImg = torch.from_numpy(tmpImg)
 
-            # inference
-            d1,d2,d3,d4,d5,d6,d7= net(tmpImg)
+                # convert numpy array to torch tensor
+                tmpImg = tmpImg.type(torch.FloatTensor)
+                tmpImg = tmpImg.to(self.device)
+            #     if torch.cuda.is_available():
+            #         tmpImg = Variable(tmpImg.cuda())
+            #     else:
+                tmpImg = Variable(tmpImg)
 
-            # normalization
-            pred = 1.0 - d1[:,0,:,:]
-            pred = normPRED(pred)
+                # inference
+                d1,d2,d3,d4,d5,d6,d7= net(tmpImg)
 
-            # convert torch tensor to numpy array
-            pred = pred.squeeze()
-            pred = pred.cpu().data.numpy()
+                # normalization
+                pred = 1.0 - d1[:,0,:,:]
+                pred = normPRED(pred)
 
-            del d1,d2,d3,d4,d5,d6,d7
+                # convert torch tensor to numpy array
+                pred = pred.squeeze()
+                pred = pred.cpu().data.numpy()
 
-            img = (pred*255).astype(np.uint8)
-            #대비 조정
-            a = 1
-            factor = 1.0 - a*0.1
-            avg = np.mean(img) / 2.0
-            avg = 255*0.5
+                del d1,d2,d3,d4,d5,d6,d7
 
-            dim = 255/(255*factor - avg)
-            img = img*factor - avg #306 - 200 = 106
+                img = (pred*255).astype(np.uint8)
+                #대비 조정
+                a = 1
+                factor = 1.0 - a*0.1
+                avg = np.mean(img) / 2.0
+                avg = 255*0.5
 
-            img[img < 0] = 0
-            img[img > 255] = 255
-            img *= dim
-            dst = img.astype(np.uint8)
+                dim = 255/(255*factor - avg)
+                img = img*factor - avg #306 - 200 = 106
 
-            # Save the file to the folder
-            cv2.imwrite(f"{save_path}/{os.path.basename(im_file)}", dst)
+                img[img < 0] = 0
+                img[img > 255] = 255
+                img *= dim
+                dst = img.astype(np.uint8)
 
-        print("Sketches are obtained!")
-    
-def create_video(path, save_path, save_name):
-    
-    import cv2, os, numpy as np
-    from glob import glob
-    from pathlib import Path
-    from tqdm import tqdm
+                # Save the file to the folder
+                cv2.imwrite(f"{self.sketch_save_path}/{os.path.basename(im_file)}", dst)
 
-    os.makedirs(save_path, exist_ok = True)
+            print("Sketches are obtained!")
 
-    ims_array = []
-    files = glob(f"{path}/*.jpg")
-    files.sort(key = os.path.getctime)
-    
-    print("Creating video from the sketch frames...")
-    for idx, filename in tqdm(enumerate(files)):
-        im = cv2.imread(filename)
-        height, width, layers = im.shape
-        ims_array.append(im) 
+    def create_video(self):
 
-    video = cv2.VideoWriter(f"{save_path}/video_{save_name}", cv2.VideoWriter_fourcc(*'DIVX'), 25, (ims_array[0].shape[1], ims_array[0].shape[0]))
+        os.makedirs(self.video_save_path, exist_ok = True)
 
-    for idx, im in enumerate(ims_array): video.write(im)
-    video.release()
-    
-    print("Video is successfully saved!")
+        ims_array = []
+        files = glob(f"{self.sketch_save_path}/*.jpg")
+        files.sort(key = os.path.getctime)
+
+        print("Creating video from the sketch frames...")
+        for idx, filename in tqdm(enumerate(files)):
+            im = cv2.imread(filename)
+            height, width, layers = im.shape
+            ims_array.append(im) 
+
+        video = cv2.VideoWriter(f"{self.video_save_path}/video_{self.save_name}", cv2.VideoWriter_fourcc(*'DIVX'), 25, (ims_array[0].shape[1], ims_array[0].shape[0]))
+
+        for idx, im in enumerate(ims_array): video.write(im)
+        video.release()
+
+        print("Video is successfully saved!")
