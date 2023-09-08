82aff9e9d61608ebc4c06a06713535190fbddcc5
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-09-08 11:29:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def run(args):
     start_time = time()
     folder_name = f"{os.path.basename(args.root).split('.')[0]}_u2"
     get_video_frames(video_path = args.root, save_path = f"{args.frames}/{folder_name}")
-    get_sketches(path = f"{args.frames}/{folder_name}", save_path = f"{args.sketches}/{folder_name}", device = "cuda")
+    get_sketches(path = f"{args.frames}/{folder_name}", save_path = f"{args.sketches}/{folder_name}", checkpoint = args.checkpoint, device = "cuda")
     create_video(path = f"sketches/{folder_name}", save_path = "videos", save_name = f"{folder_name}.mp4")
     print(f"The process is done in {(time() - start_time):.3f} seconds!")
     
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     
     # Add arguments to the parser
     parser.add_argument("-r", "--root", type = str, default = "../pencil_sketches/potato.mp4", help = "Path to the video")
+    parser.add_argument("-cp", "--checkpoint", type = str, default = "saved_models/u2net_portrait/u2net_portrait.pth", help = "Path to trained model checkpoint")
     parser.add_argument("-f", "--frames", type = str, default = "frames", help = "Path to save the frames")
     parser.add_argument("-s", "--sketches", type = str, default = "sketches", help = "Path to save the sketches")
     parser.add_argument("-sp", "--save_path", type = str, default = "videos", help = "Path to save the video")
