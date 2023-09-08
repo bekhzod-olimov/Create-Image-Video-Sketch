62b6aa1ca50df2f4c29783d0a10d21ef593d4982
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-09-08 12:10:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os, argparse
-from utils import get_video_frames, get_sketches, create_video
+from utils import VideoMaker
 from time import time
 
 def run(args):    
 
     start_time = time()
     folder_name = f"{os.path.basename(args.root).split('.')[0]}_u2"
-    get_video_frames(video_path = args.root, save_path = f"{args.frames}/{folder_name}")
-    get_sketches(path = f"{args.frames}/{folder_name}", save_path = f"{args.sketches}/{folder_name}", checkpoint = args.checkpoint, device = "cuda")
-    create_video(path = f"sketches/{folder_name}", save_path = "videos", save_name = f"{folder_name}.mp4")
+    
+    vid = VideoMaker(args.root, f"{args.frames}/{folder_name}", f"{args.sketches}/{folder_name}",
+                     device = "cuda", checkpoint = args.checkpoint, video_save_path = args.save_path,
+                     save_name = f"{folder_name}.mp4")
+    
+    vid.get_video_frames(); vid.get_sketches(); vid.create_video()
     print(f"The process is done in {(time() - start_time):.3f} seconds!")
     
 if __name__ == "__main__":
@@ -17,7 +20,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Sketch Video Create Arguments")
     
     # Add arguments to the parser
-    parser.add_argument("-r", "--root", type = str, default = "../pencil_sketches/potato.mp4", help = "Path to the video")
+    parser.add_argument("-r", "--root", type = str, default = "potato.mp4", help = "Path to the video")
     parser.add_argument("-cp", "--checkpoint", type = str, default = "saved_models/u2net_portrait/u2net_portrait.pth", help = "Path to trained model checkpoint")
     parser.add_argument("-f", "--frames", type = str, default = "frames", help = "Path to save the frames")
     parser.add_argument("-s", "--sketches", type = str, default = "sketches", help = "Path to save the sketches")
